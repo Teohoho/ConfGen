b4024a3f907fae7879e5bde082df22835c20b385
teohoho <teo.sulea@gmail.com>
2022-05-11 15:02:12 +0300

    Replaced elliptical generation with a surface-based generation method.

--- ConfGen/Search.py
+++ ConfGen/Search.py
@@ -9,18 +9,15 @@ def centroidPoints(arr):
     sum_z = np.sum(arr[:, 2])
     return np.array([sum_x/length, sum_y/length, sum_z/length])
 
-def ellipticalOrbit(theta,ratio):
-    ETM = np.array([[np.cos(theta), 0, -np.sqrt(ratio)*np.sin(theta)],
-                    [0,1,0],
-                    [ratio**(-1)*np.sin(theta), 0, np.cos(theta)]])
+def circularOrbit(theta):
+    CTM = np.array([[np.cos(theta), 0, (-1)*np.sin(theta)],
+                    [0, 1, 0],
+                    [np.sin(theta),0,np.cos(theta)]])
 
-    return (ETM)
+    return (CTM)
 
-def Search(fixedSystem, mobileSystem, vdWoffset, theta=45, phi=45, delta=0.27, ndelta=0):
+def SearchSurface(fixedSystem, mobileSystem, vdWoffset=0, theta=45, phi=45, delta=0.27, ndelta=0):
     """
-    Function that searches the conformational space by translating and rotating
-    relative to the Y-axis
-
     Parameters
     ----------
     fixedSystem:    numpy.ndarray
@@ -31,97 +28,77 @@ def Search(fixedSystem, mobileSystem, vdWoffset, theta=45, phi=45, delta=0.27, n
                     array, shape (N,3), of the rotated positions
                     of the two systems. in nm!
 
-    vdWoffset:       float
+    vdWoffset:      float
                     how many nm to add to the offset, to account for
                     vdW interactions between the systems.
 
     theta:          float
                     increment by which to increase the rotation of
                     sys2 relative to the origin, in degrees
+
     phi:            float
                     increment by which to increase the rotation of
                     sys2 relative to itself, in degrees
     delta:          float
                     increment by which to translate sys2 relative
                     to the origin
-    ndelta:         int
+    ndelta          int
                     how many times to translate sys2 by +/- delta
                     relative to the origin
 
     Returns
     -------
-    conformations: numpy.ndarray
-                   array of dimensions (M,N,3), where:
-                   *)   M is the number of conformations found, which should be equal to
-                   (360/theta + 360/phi + ndelta + 1)
-                   *)   N is the number of particles in the system
-                   *)   3 is the integer that comes after 2, but before 4.
+    conformations:  numpy.ndarray
+                    array of dimensions (M,N,3), where:
+                    *)   M is the number of conformations found, which should be equal to
+                    (360/theta + 360/phi + ndelta + 1)
+                    *)   N is the number of particles in the system
+                    *)   3 is the integer that comes after 2, but before 4.
 
     Notes
     -----
     By default, we initially increment along the z axis, then translate
     along the Y axis (by delta), then we rotate in the XZ plane
-    (by both theta and phi)
+    (by both theta and phi). Afterwards we bring the mobile system
+    closer to the origin so the contacts made between the two systems are tight.
+
     This class works on the assumption that the system was previously centered in
     the (0,0,0) coordinate. This can be done via the AlignSystem class, found
     in this module.
-    r1 and r2 are the larger and respectively smaller distance between the (0,0,0)
-    point and the points with the largest X/Z coordinate
     """
 
     conformations = np.zeros((0, mobileSystem.shape[0], 3))
+    # Get the coordinates of the point farthest from the origin in the XZ plane for
+    # both systems
+    atomDistances_FS = np.zeros((fixedSystem.shape[0]))
+    for atomIx in range(fixedSystem.shape[0]):
+        atomDistances_FS[atomIx] = np.linalg.norm(np.array(fixedSystem[atomIx][0],fixedSystem[atomIx][2]))
+    maxDist_FS = np.max(atomDistances_FS)
 
-    # Get the coordinates of the point with the largest X and Z
-    # coordinate for FixedSystem and MobileSystem
-    print ("\n{0} DEBUG INFO {0}".format(20*"#"))
-    FS_X = fixedSystem[np.argmax(np.abs(fixedSystem[:,0]))]
-    FS_Z = fixedSystem[np.argmax(np.abs(fixedSystem[:,2]))]
-    MS_X = mobileSystem[np.argmax(np.abs(mobileSystem[:,0]))]
-    MS_Z = mobileSystem[np.argmax(np.abs(mobileSystem[:,2]))]
+    atomDistances_MS = np.zeros((mobileSystem.shape[0]))
+    for atomIx in range(mobileSystem.shape[0]):
+        atomDistances_MS[atomIx] = np.linalg.norm(np.array(mobileSystem[atomIx][0], mobileSystem[atomIx][2]))
+    maxDist_MS = np.max(atomDistances_MS)
+    offset = maxDist_FS + maxDist_MS + vdWoffset
 
-    print ("FS_X: {} (index {})".format(FS_X,np.argmax(np.abs(fixedSystem[:,0]))))
-    print ("FS_Z: {} (index {})".format(FS_Z,np.argmax(np.abs(fixedSystem[:,2]))))
-    print ("MS_X: {} (index {})".format(MS_X,np.argmax(np.abs(mobileSystem[:,0]))))
-    print ("MS_Z: {} (index {})".format(MS_Z,np.argmax(np.abs(mobileSystem[:,2]))))
-
-    # Get the distance between the points and the origin and define
-    # r1 and r2 (in XZ plane only!)
-    FS_XDist = np.linalg.norm(np.array([FS_X[0],FS_X[2]]))
-    FS_ZDist = np.linalg.norm(np.array([FS_Z[0],FS_Z[2]]))
-    MS_XDist = np.linalg.norm(np.array([MS_X[0],MS_X[2]]))
-    MS_ZDist = np.linalg.norm(np.array([MS_Z[0],MS_Z[2]]))
-
-    print ("FS_XDist: {}".format(FS_XDist))
-    print ("FS_ZDist: {}".format(FS_ZDist))
-    print ("MS_XDist: {}".format(MS_XDist))
-    print ("MS_ZDist: {}".format(MS_ZDist))
-
-    FS_R1 = max([FS_XDist,FS_ZDist])
-    FS_R2 = min([FS_XDist,FS_ZDist])
-    MS_R1 = max([MS_XDist,MS_ZDist])
-    MS_R2 = min([MS_XDist,MS_ZDist])
-
-    print ("FS_R1: {}".format(FS_R1))
-    print ("FS_R2: {}".format(FS_R2))
-    print ("MS_R1: {}".format(MS_R1))
-    print ("MS_R2: {}".format(MS_R2))
-
-    LongAxis = FS_R1 + FS_R2
-    ShortAxis = max([FS_R1 + MS_R2, MS_R1 + FS_R2])
-    AxesRatio = LongAxis/ShortAxis
-
-    print ("Long Axis: {}".format(LongAxis))
-    print ("Short Axis: {}".format(ShortAxis))
-    print ("Axes Ratio: {}".format(AxesRatio))
-
-    offset = np.sum(LongAxis,vdWoffset)
-    print ("Offset: {}".format(offset))
-    print ("{0} DEBUG INFO END {0}\n".format(16*"#"))
-
+    print("\n{0} DEBUG INFO {0}".format(20 * "#"))
+    #print("Farthest atom FS {}: {}nm".format(atomFar,maxDist_FS))
+    #print("Farthest atom MS {}: {}nm".format(atomFar,maxDist_MS))
+    #print("maxDist_FS + maxDist_MS: {}".format(maxDist_FS + maxDist_MS))
+    #print("maxDist_FS + maxDist_MS + vdWoffset: {}".format(offset))
 
     # Set up number of rotations
-    thetaIterations = np.int(360/theta)
-    phiIterations = np.int(360/phi)
+    if (theta == 0):
+        theta = 360
+    if (phi == 0):
+        phi = 360
+
+    thetaIterations = np.int(360 / theta)
+    phiIterations = np.int(360 / phi)
+
+    print ("theta Iterations: {}".format(thetaIterations))
+    print ("phi Iterations: {}".format(phiIterations))
+    print ("delta Iterations: {}".format((2*ndelta)+1))
 
     # Display warning if theta/phi are not divisors of 360
     if (360 % theta != 0 or 360 % phi != 0):
@@ -135,26 +112,51 @@ def Search(fixedSystem, mobileSystem, vdWoffset, theta=45, phi=45, delta=0.27, n
         for rotIx in range(phiIx):
             sys2 = np.array(phi_vector.apply(sys2))
 
-    # Translate along the selected axis
-        sys2 = sys2 + np.array(np.array([1,0,0]) * offset)
+        # Translate along the X axis
+        #print ("sys2: {}".format(sys2))
+        sys2 = sys2 + (np.array([1, 0, 0]) * offset)
+        #print ("sys2: {}".format(sys2))
+        #print ("sys2_CM: {}".format(centroidPoints(sys2)))
 
-    # Translate by delta
-        for i in range(-ndelta, ndelta+1):
+        # Translate by delta
+        for i in range(-ndelta, ndelta + 1):
             sys3 = sys2
-            sys3 = sys3 + (np.array([0,1,0])*i*delta)
+            sys3 = sys3 + (np.array([0, 1, 0]) * i * delta)
 
             # Define a center point which we move in an elliptical orbit. We don't
             # move the entire protein to avoid the skewing that comes with an
             # elliptical motion
-            centralPoint = np.array([1,0,0]) * offset
+            centralPoint = np.array([1, 0, 0]) * offset
+            #print (centralPoint)
 
             for thetaIx in range(thetaIterations):
-                # Define the elliptical TM
+                # Define the circular TM
                 sys4 = np.zeros(sys3.shape)
-                ellipticalTM = ellipticalOrbit(np.radians(theta*thetaIx), AxesRatio)
-                movedPoint = np.matmul(ellipticalTM,centralPoint)
+                #print ("theta * thetaIx: {}".format(theta * thetaIx))
+                #print ("sin theta: {}".format(np.sin(theta * thetaIx)))
+                #print ("cos theta: {}".format(np.cos(theta * thetaIx)))
+                circularTM = circularOrbit(np.radians(theta * thetaIx))
+                movedPoint = np.matmul(circularTM, centralPoint)
+                #print (movedPoint)
                 for atomIx in range(sys3.shape[0]):
                     sys4[atomIx] = sys3[atomIx] + movedPoint - centralPoint
-                conformations = np.concatenate((conformations,np.array(sys4,ndmin=3)))
 
+                # In order to better approximate the surface of the fixed system, we
+                # bring the mobile system closer to the origin by a distance equal to the minimum
+                # distance between the two helices minus the vdWoffset
+                distMin = 9999
+                for FS_AtomIX in range(fixedSystem.shape[0]):
+                    for MS_AtomIX in range(mobileSystem.shape[0]):
+                        distFS_MS = np.linalg.norm(fixedSystem[FS_AtomIX] - sys4[MS_AtomIX])
+                        if (distFS_MS < distMin):
+                            distMin = distFS_MS
+                print("Dist min: {}".format(distMin))
+                distMin = distMin - vdWoffset
+                angle = np.radians(theta * thetaIx)
+                sys4 = sys4 - [distMin*np.cos(angle),0,distMin*np.sin(angle)]
+
+
+                conformations = np.concatenate((conformations, np.array(sys4, ndmin=3)))
+    print ("Conformations.shape: {}".format(conformations.shape))
+    print("{0} DEBUG INFO END {0}\n".format(18 * "#"))
     return (conformations)
